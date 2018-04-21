9962a2627711f73614effdae8d68f2c70f62584b
linxi <lx02321@163.com>
2018-04-21 11:08:31 +0800

    Initial commit

---
+++ src/components/childTemplate/childComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="child">{{msg}}{{message}}</div>
+    <button @click="changeMsgs">click</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "child-component",
+    data() {
+      return {
+        msg: "我是子模版"
+      }
+    },
+    watch:{
+      msg:function (val,oldVal) {
+
+      }
+    },
+    methods:{
+      changeMsgs(){
+        this.msg="我还是一个子模版!";
+      }
+    },
+    props:[
+      'message'
+    ]
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/firstComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="first">
+    <router-link to="/">我是第一个模版</router-link>
+    <button @click="changeMsg">click</button>
+    <child-component :message='message'></child-component>
+  </div>
+</template>
+import childComponent from ''
+<script>
+  import childComponent from './childTemplate/childComponent'
+
+  export default {
+    name: "first-component",
+    data(){
+      return {
+        message:"爸爸给的!",
+      }
+    },
+    methods:{
+      changeMsg(){
+        this.message="爸爸不给了!";
+      }
+    },
+    components: {
+      childComponent
+    }
+  }
+</script>
+
+<style scoped>
+  .first {
+    color: #42b983;
+  }
+</style>
